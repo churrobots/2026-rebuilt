fe7e0c6e607e501aed04051676a07b47e454bf1f
Churrobots Team <55808130+epa-robotics-team@users.noreply.github.com>
2026-03-10 09:06:30 -0700

    missed an import

--- src/main/java/frc/robot/subsystems/Spindexer.java
+++ src/main/java/frc/robot/subsystems/Spindexer.java
@@ -14,6 +14,7 @@ import java.util.function.Supplier;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
